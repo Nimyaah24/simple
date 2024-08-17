611cad39bcf1bac4815692f400f2794087806297
Nimyaah24 <nimyapappy823@gmail.com>
2024-08-17 20:01:24 +0530

    achan

--- src/App.jsx
+++ src/App.jsx
@@ -36,32 +36,32 @@ let reaload =()=>{
   return (
     <>
       
-     <div  className='d-flex justify-content-center p-4  aligin-items-center mt-5'>
+     <div  className='d-flex justify-content-center p-4  aligin-items-center mt-3' s>
 
-     <div className='bg-transparent shadow-xl shadow-black shadow p-5 rounded' style={{ width: "350px" }}>
+     <div className='bg-secondary shadow-xl shadow-black shadow p-3  rounded' style={{ width: "350px" }}>
 
-     <h3><span style={{color:"brown"}} >BMI</span> Calculator </h3>
+     <h3 className='text-center '>BMI <span className='text-danger' >Calculator </span></h3>
   
         <form onSubmit={findBMI} className='mt-4 '>
           <div>
-           <h6>Height <span className='text-secondary'>(in)</span></h6>
-            <input value={height || ""} onChange={(e)=>setHeight(e.target.value)}   type="text" placeholder='enter your height' className='form-control' />
+           <h6>Height</h6>
+            <input value={height || ""} onChange={(e)=>setHeight(e.target.value)}   type="text" placeholder='enter height' className='form-control' />
             
           </div>
           <div>
-          <h6 className='mt-3'>Weight <span className='text-secondary'>(kg)</span></h6>
-          <input type="text"  value={Weight || ""} onChange={(e)=>setWeight(e.target.value)} placeholder='enter your Weight' className='form-control' />
+          <h6 className='mt-3'>Weight </h6>
+          <input type="text"  value={Weight || ""} onChange={(e)=>setWeight(e.target.value)} placeholder='enter Weight' className='form-control' />
           </div>
           <div className='d-flex p-3 ' >
-            <button  className=' btn1 btn-secondary rounded mt-3 ms-2 me-4 '  style={{width:"40%"}}>Calculate</button>
-            <button onClick={reaload} className='btn btn-dark rounded mt-3 ' style={{width:"40%"}}>Reload</button>
+            <button  className=' btn1 btn-success rounded mt-3 ms-2 me-4 '  style={{width:"40%"}}>Calculate</button>
+            <button onClick={reaload} className='btn btn-primary rounded mt-3 ' style={{width:"40%"}}>Clear</button>
           </div>
         </form>
 
 
 
 <div className='output mt-3'>
-<h6>Your BMI is : </h6>
+<h6>BMI is : </h6>
 <p id='bi'>{bmi}</p>
 <h5 style={{color:"brown"}}>{msg}</h5>
 </div>
